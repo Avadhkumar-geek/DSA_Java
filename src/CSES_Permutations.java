187b238ca4dff0acbb0e5ce09849e1677c214824
Avadhkumar-geek <avadhkachhadiya@gmail.com>
2022-07-02 19:53:36 +0530

    added some CSES introductory problem sets solution

---
+++ src/CSES_Permutations.java
@@ -0,0 +1,159 @@
+import java.util.*;
+import java.io.*;
+
+public class CSES_Permutations {
+
+// ========================== CODE STARTS HERE ===================================================
+
+
+    public static void solve(InputReader in, PrintWriter out) {
+        int n = in.nextInt();
+
+        if (n == 1) {
+            out.println(1);
+        } else if (n <= 3) {
+            out.println("NO SOLUTION");
+        } else {
+            int even_max, odd_max;
+
+            if (n % 2 == 0) {
+                even_max = n;
+                odd_max = n - 1;
+            } else {
+                even_max = n - 1;
+                odd_max = n;
+            }
+
+            while (odd_max > 0) {
+                out.print(odd_max + " ");
+                odd_max -= 2;
+            }
+
+            while (even_max > 0) {
+                out.print(even_max + " ");
+                even_max -= 2;
+            }
+        }
+    }
+
+
+// =========================== CODE ENDS HERE ====================================================
+
+
+    public static void main(String[] args) throws IOException {
+        InputReader in = new InputReader();
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        solve(in, out);
+        out.close();
+    }
+
+    static class InputReader {
+        private byte[] buf = new byte[16384];
+        private int curChar;
+        private int numChars;
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = System.in.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public String nextLine() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isEndOfLine(c));
+            return res.toString();
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] arr = new long[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextLong();
+            }
+            return arr;
+        }
+
+        private boolean isSpaceChar(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        private boolean isEndOfLine(int c) {
+            return c == '\n' || c == '\r' || c == -1;
+        }
+    }
+}
