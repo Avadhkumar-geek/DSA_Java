a03554083602dc20f0aac1be0e8f9f2c0cd675ec
Avadhkumar-geek <avadhkachhadiya@gmail.com>
2022-07-16 18:43:29 +0530

    updated

--- src/CSES/FerrisWheel.java
+++ src/CSES/FerrisWheel.java
@@ -5,40 +5,29 @@ import static java.lang.System.out;
 import java.util.*;
 import java.io.*;
 
-public class Apartments {
+public class FerrisWheel {
 
     // ============================ CODE STARTS HERE ============================
 
     public static void main(String[] args) throws Exception {
-        FastReader in = new FastReader();
-        int n = in.nextInt();
-        int m = in.nextInt();
-        int k = in.nextInt();
-
-
-        PriorityQueue<Integer> arr = new PriorityQueue<>(n);
-        for (int i = 0; i < n; i++) {
-            arr.add(in.nextInt());
-        }
-
-        PriorityQueue<Integer> brr = new PriorityQueue<>(m);
-        for (int i = 0; i < m; i++) {
-            brr.add(in.nextInt());
-        }
-
+        FastScanner in = new FastScanner();
+        int c = in.nextInt();
+        int weight = in.nextInt();
+        int[] childs = in.nextInts(c);
         int cnt = 0;
-        while (!arr.isEmpty() && !brr.isEmpty()) {
-            if ((brr.peek() >= arr.peek() - k) && (brr.peek() <= arr.peek() + k)) {
-                arr.poll();
-                brr.poll();
-                cnt++;
-            } else if (arr.peek() + k < brr.peek()) {
-                arr.poll();
-            } else if (arr.peek() - k > brr.peek()) {
-                brr.poll();
+
+        sort(childs);
+        int f = 0;
+        int l = childs.length - 1;
+        while (f <= l) {
+            if (childs[f] + childs[l] <= weight) {
+                f++;
             }
+            l--;
+            cnt++;
         }
-        System.out.println(cnt);
+
+        out.println(cnt);
 
     }
 
@@ -156,49 +145,115 @@ public class Apartments {
     }
 }
 
-class FastReader {
-    BufferedReader br;
-    StringTokenizer st;
+class FastScanner {
+    //I don't understand how this works lmao
+    private int BS = 1 << 16;
+    private char NC = (char) 0;
+    private byte[] buf = new byte[BS];
+    private int bId = 0, size = 0;
+    private char c = NC;
+    private double cnt = 1;
+    private BufferedInputStream in;
 
-    public FastReader() {
-        br = new BufferedReader(
-                new InputStreamReader(System.in));
+    public FastScanner() {
+        in = new BufferedInputStream(System.in, BS);
     }
 
-    String next() {
-        while (st == null || !st.hasMoreElements()) {
-            try {
-                st = new StringTokenizer(br.readLine());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return st.nextToken();
-    }
-
-    int nextInt() {
-        return Integer.parseInt(next());
-    }
-
-    long nextLong() {
-        return Long.parseLong(next());
-    }
-
-    double nextDouble() {
-        return Double.parseDouble(next());
-    }
-
-    String nextLine() {
-        String str = "";
+    public FastScanner(String s) {
         try {
-            if (st.hasMoreTokens()) {
-                str = st.nextToken("\n");
-            } else {
-                str = br.readLine();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+            in = new BufferedInputStream(new FileInputStream(new File(s)), BS);
+        } catch (Exception e) {
+            in = new BufferedInputStream(System.in, BS);
         }
-        return str;
+    }
+
+    private char getChar() {
+        while (bId == size) {
+            try {
+                size = in.read(buf);
+            } catch (Exception e) {
+                return NC;
+            }
+            if (size == -1) return NC;
+            bId = 0;
+        }
+        return (char) buf[bId++];
+    }
+
+    public int nextInt() {
+        return (int) nextLong();
+    }
+
+    public int[] nextInts(int N) {
+        int[] res = new int[N];
+        for (int i = 0; i < N; i++) {
+            res[i] = (int) nextLong();
+        }
+        return res;
+    }
+
+    public long[] nextLongs(int N) {
+        long[] res = new long[N];
+        for (int i = 0; i < N; i++) {
+            res[i] = nextLong();
+        }
+        return res;
+    }
+
+    public long nextLong() {
+        cnt = 1;
+        boolean neg = false;
+        if (c == NC) c = getChar();
+        for (; (c < '0' || c > '9'); c = getChar()) {
+            if (c == '-') neg = true;
+        }
+        long res = 0;
+        for (; c >= '0' && c <= '9'; c = getChar()) {
+            res = (res << 3) + (res << 1) + c - '0';
+            cnt *= 10;
+        }
+        return neg ? -res : res;
+    }
+
+    public double nextDouble() {
+        double cur = nextLong();
+        return c != '.' ? cur : cur + nextLong() / cnt;
+    }
+
+    public double[] nextDoubles(int N) {
+        double[] res = new double[N];
+        for (int i = 0; i < N; i++) {
+            res[i] = nextDouble();
+        }
+        return res;
+    }
+
+    public String next() {
+        StringBuilder res = new StringBuilder();
+        while (c <= 32) c = getChar();
+        while (c > 32) {
+            res.append(c);
+            c = getChar();
+        }
+        return res.toString();
+    }
+
+    public String nextLine() {
+        StringBuilder res = new StringBuilder();
+        while (c <= 32) c = getChar();
+        while (c != '\n') {
+            res.append(c);
+            c = getChar();
+        }
+        return res.toString();
+    }
+
+    public boolean hasNext() {
+        if (c > 32) return true;
+        while (true) {
+            c = getChar();
+            if (c == NC) return false;
+            else if (c > 32) return true;
+        }
     }
 }
