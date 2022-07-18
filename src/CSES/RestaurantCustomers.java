2a22342e15c9b27f6c2e0a66a479b94b150b29ab
Avadhkumar-geek <avadhkachhadiya@gmail.com>
2022-07-18 23:31:02 +0530

    updated

--- src/CSES/RestaurantCustomers.java
+++ src/CSES/RestaurantCustomers.java
@@ -5,20 +5,30 @@ import static java.lang.System.out;
 import java.util.*;
 import java.io.*;
 
-public class DistinctNumbers {
+public class RestaurantCustomers {
 
     // ============================ CODE STARTS HERE ============================
 
     public static void main(String[] args) throws Exception {
-        FastReader in = new FastReader();
-        int s = in.nextInt();
-        Set<Long> arr = new HashSet<>(s);
-        for (int i = 0; i < s; i++) {
-            arr.add(in.nextLong());
+        FastScanner in = new FastScanner();
+        int T = in.nextInt();
+        TreeMap<Integer, Integer> time = new TreeMap<>();
+        int a,b;
+        for (int i = 0; i < T; i++) {
+            a = in.nextInt();
+            b = in.nextInt();
+            time.put(a, 1);
+            time.put(b, -1);
         }
 
-        out.println(arr.size());
 
+
+        int s = 0, res = 0;
+        for (Map.Entry<Integer, Integer> i: time.entrySet()) {
+            s += i.getValue();
+            res = Math.max(s, res);
+        }
+        out.println(res);
     }
 
     // ============================ CODE STARTS HERE ============================
@@ -134,50 +144,116 @@ public class DistinctNumbers {
         return res;
     }
 
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
+    static class FastScanner {
+        //I don't understand how this works lmao
+        private int BS = 1 << 16;
+        private char NC = (char) 0;
+        private byte[] buf = new byte[BS];
+        private int bId = 0, size = 0;
+        private char c = NC;
+        private double cnt = 1;
+        private BufferedInputStream in;
 
-        public FastReader() {
-            br = new BufferedReader(
-                    new InputStreamReader(System.in));
+        public FastScanner() {
+            in = new BufferedInputStream(System.in, BS);
         }
 
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
+        public FastScanner(String s) {
             try {
-                if (st.hasMoreTokens()) {
-                    str = st.nextToken("\n");
-                } else {
-                    str = br.readLine();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+                in = new BufferedInputStream(new FileInputStream(new File(s)), BS);
+            } catch (Exception e) {
+                in = new BufferedInputStream(System.in, BS);
             }
-            return str;
+        }
+
+        private char getChar() {
+            while (bId == size) {
+                try {
+                    size = in.read(buf);
+                } catch (Exception e) {
+                    return NC;
+                }
+                if (size == -1) return NC;
+                bId = 0;
+            }
+            return (char) buf[bId++];
+        }
+
+        public int nextInt() {
+            return (int) nextLong();
+        }
+
+        public int[] nextInts(int N) {
+            int[] res = new int[N];
+            for (int i = 0; i < N; i++) {
+                res[i] = (int) nextLong();
+            }
+            return res;
+        }
+
+        public long[] nextLongs(int N) {
+            long[] res = new long[N];
+            for (int i = 0; i < N; i++) {
+                res[i] = nextLong();
+            }
+            return res;
+        }
+
+        public long nextLong() {
+            cnt = 1;
+            boolean neg = false;
+            if (c == NC) c = getChar();
+            for (; (c < '0' || c > '9'); c = getChar()) {
+                if (c == '-') neg = true;
+            }
+            long res = 0;
+            for (; c >= '0' && c <= '9'; c = getChar()) {
+                res = (res << 3) + (res << 1) + c - '0';
+                cnt *= 10;
+            }
+            return neg ? -res : res;
+        }
+
+        public double nextDouble() {
+            double cur = nextLong();
+            return c != '.' ? cur : cur + nextLong() / cnt;
+        }
+
+        public double[] nextDoubles(int N) {
+            double[] res = new double[N];
+            for (int i = 0; i < N; i++) {
+                res[i] = nextDouble();
+            }
+            return res;
+        }
+
+        public String next() {
+            StringBuilder res = new StringBuilder();
+            while (c <= 32) c = getChar();
+            while (c > 32) {
+                res.append(c);
+                c = getChar();
+            }
+            return res.toString();
+        }
+
+        public String nextLine() {
+            StringBuilder res = new StringBuilder();
+            while (c <= 32) c = getChar();
+            while (c != '\n') {
+                res.append(c);
+                c = getChar();
+            }
+            return res.toString();
+        }
+
+        public boolean hasNext() {
+            if (c > 32) return true;
+            while (true) {
+                c = getChar();
+                if (c == NC) return false;
+                else if (c > 32) return true;
+            }
         }
     }
 }
